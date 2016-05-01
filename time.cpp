737cb4669b5c31c918bc2c1be52d6a123cd5aa68
Jingwei Zhang <Jingwei Zhang>
2016-04-30 23:47:43 -0400

    we can use this to display time

---
+++ time.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<ctime>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    struct tm *ptr;
+    time_t lt;
+    lt =time(NULL);
+    ptr=gmtime(&lt);
+    printf(ctime(&lt));
+    return 0;
+}
